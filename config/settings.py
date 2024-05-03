cccdd5b58abcc562c127d7820e390fcda35c1c79
Gladkov Sergey <melcor2006@mail.ru>
2024-05-03 18:48:35 +0300

    1 task

--- config/settings.py
+++ config/settings.py
@@ -73,8 +73,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
 	'default': {
-		'ENGINE': 'django.db.backends.sqlite3',
-		'NAME': BASE_DIR / 'db.sqlite3',
+		'ENGINE': 'django.db.backends.postgresql',
+		'NAME': 'catalog',
+		'HOST': '127.0.0.1',
+		'USER': 'postgres',
+		'PASSWORD': '32167',
+		'PORT': '5432',
 	}
 }
 
