98aa6a77052d81473fedb77a8700fc31c6d81287
Gladkov Sergey <melcor2006@mail.ru>
2024-05-03 19:24:55 +0300

    3 task

--- catalog/models.py
+++ catalog/models.py
@@ -1,22 +1,34 @@
 from django.db import models
 
+NULLABLE = {'blank': 'True', 'null': 'True'}
+
 
 class Product(models.Model):
-	pass
+	name = models.CharField(max_length=50, verbose_name='наименование')
+	description = models.TextField(verbose_name='описание')
+	image = models.ImageField(upload_to='media/photo', **NULLABLE)
+	category = models.ForeignKey(on_delete=models.SET_NULL, verbose_name='категория',  **NULLABLE, related_name='products')
+	price = models.IntegerField(verbose_name='цена')
+	created_at = models.DateField(auto_created=True, verbose_name='дата создания')
+	updated_at = models.DateField(auto_now_add=True, verbose_name='дата изменения')
 
 	def __str__(self):
-		pass
+		return self.name, self.description
 
 	class Meta:
-		pass
+		verbose_name = 'продукт'
+		verbose_name_plural = 'продукты'
+		ordering = ('name', 'category')
 
 
 class Category(models.Model):
-	pass
+	name = models.CharField(max_length=50, verbose_name='наименование')
+	description = models.TextField(verbose_name='описание')
 
 	def __str__(self):
-		pass
+		return self.name, self.description
 
 	class Meta:
-		pass
-
+		verbose_name = 'категория'
+		verbose_name_plural = 'категории'
+		ordering = ('name',)
