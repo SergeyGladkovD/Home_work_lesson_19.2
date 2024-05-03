f46a74d29584a18919fc5031c81de4ac741084d3
Gladkov Sergey <melcor2006@mail.ru>
2024-05-03 21:13:06 +0300

    5 task

--- catalog/models.py
+++ catalog/models.py
@@ -5,7 +5,7 @@ NULLABLE = {'blank': 'True', 'null': 'True'}
 
 class Product(models.Model):
 	name = models.CharField(max_length=50, verbose_name='наименование')
-	description = models.TextField(verbose_name='описание')
+	description = models.TextField(verbose_name='описание', **NULLABLE)
 	image = models.ImageField(upload_to='media/photo', **NULLABLE)
 	category = models.ForeignKey('Category', on_delete=models.SET_NULL, verbose_name='категория',  **NULLABLE, related_name='products')
 	price = models.IntegerField(verbose_name='цена')
@@ -13,7 +13,7 @@ class Product(models.Model):
 	updated_at = models.DateField(auto_now_add=True, verbose_name='дата изменения')
 
 	def __str__(self):
-		return self.name, self.description
+		return f'{self.name} {self.description} {self.price}'
 
 	class Meta:
 		verbose_name = 'продукт'
@@ -26,7 +26,7 @@ class Category(models.Model):
 	description = models.TextField(verbose_name='описание')
 
 	def __str__(self):
-		return self.name, self.description
+		return f'{self.name} {self.description}'
 
 	class Meta:
 		verbose_name = 'категория'
