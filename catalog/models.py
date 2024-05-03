74f0e3e84e4fa2ad6125b497bc58e2ac3d2591fc
Gladkov Sergey <melcor2006@mail.ru>
2024-05-03 22:12:35 +0300

    6.1 task

--- catalog/models.py
+++ catalog/models.py
@@ -8,9 +8,9 @@ class Product(models.Model):
 	description = models.TextField(verbose_name='описание', **NULLABLE)
 	image = models.ImageField(upload_to='media/photo', **NULLABLE)
 	category = models.ForeignKey('Category', on_delete=models.SET_NULL, verbose_name='категория',  **NULLABLE, related_name='products')
-	price = models.IntegerField(verbose_name='цена')
-	created_at = models.DateField(auto_created=True, verbose_name='дата создания')
-	updated_at = models.DateField(auto_now_add=True, verbose_name='дата изменения')
+	price = models.IntegerField(verbose_name='цена', **NULLABLE)
+	created_at = models.DateField(auto_created=True, verbose_name='дата создания', **NULLABLE)
+	updated_at = models.DateField(auto_now_add=True, verbose_name='дата изменения', **NULLABLE)
 
 	def __str__(self):
 		return f'{self.name} {self.description} {self.price}'
@@ -23,7 +23,7 @@ class Product(models.Model):
 
 class Category(models.Model):
 	name = models.CharField(max_length=50, verbose_name='наименование')
-	description = models.TextField(verbose_name='описание')
+	description = models.TextField(verbose_name='описание', **NULLABLE)
 
 	def __str__(self):
 		return f'{self.name} {self.description}'
