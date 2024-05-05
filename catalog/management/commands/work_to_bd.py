7ba411ee2390361942cf62b268b156c5ee040940
Gladkov Sergey <melcor2006@mail.ru>
2024-05-05 13:40:13 +0300

    completed

--- catalog/management/commands/work_to_bd.py
+++ catalog/management/commands/work_to_bd.py
@@ -8,39 +8,42 @@ from catalog.models import Category, Product
 
 class Command(BaseCommand):
 
-	def handle(self, *args, **options):
+    def handle(self, *args, **options):
 
-		with connection.cursor() as cursor:
-			cursor.execute(f"TRUNCATE TABLE catalog_category RESTART IDENTITY CASCADE;")
+        with connection.cursor() as cursor:
+            cursor.execute(f"TRUNCATE TABLE catalog_category RESTART IDENTITY CASCADE;")
+            cursor.execute(f"TRUNCATE TABLE catalog_product RESTART IDENTITY CASCADE;")
+        Product.objects.all().delete()
+        Category.objects.all().delete()
 
-		Product.objects.all().delete()
-		Category.objects.all().delete()
+        with open("catalog.json", encoding="utf8") as file:
+            data = json.load(file)
+            catalog_category = []
+            catalog_product = []
+            for category in data:
+                if category["model"] == "catalog.category":
+                    catalog_category.append(
+                        Category(
+                            id=category["pk"],
+                            name=category["fields"]["name"],
+                            description=category["fields"]["description"],
+                        )
+                    )
 
-		with open("catalog.json", encoding="utf8") as file:
-			data = json.load(file)
-			catalog_category = []
-			catalog_product = []
-			for category in data:
-				if category["model"] == "catalog.category":
-					catalog_category.append(
-						Category(
-							name=category["fields"]["name"],
-							description=category["fields"]["description"],
-						)
-					)
+            Category.objects.bulk_create(catalog_category)
 
-			Category.objects.bulk_create(catalog_category)
+            for product in data:
+                if product["model"] == "catalog.product":
+                    catalog_product.append(
+                        Product(
+                            id=product["pk"],
+                            name=product["fields"]["name"],
+                            description=product["fields"]["description"],
+                            category=Category.objects.get(
+                                pk=product["fields"]["category"]
+                            ),
+                            price=product["fields"]["price"],
+                        ),
+                    )
 
-			for product in data:
-				if product["model"] == "catalog.product":
-					catalog_product.append(
-						Product(
-							name=product["fields"]["name"],
-							description=product["fields"]["description"],
-							category=Category.objects.get(
-								pk=product["fields"]["category"]),
-							price=product["fields"]["price"],
-							),
-						)
-
-			Product.objects.bulk_create(catalog_product)
+            Product.objects.bulk_create(catalog_product)
