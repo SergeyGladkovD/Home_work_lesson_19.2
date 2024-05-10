cc7f403e9a6aa25dfd7dafda11151eeec96f8a8a
melcor <melcor2006@mail.ru>
2024-05-10 20:22:02 +0300

    one problem

--- catalog/views.py
+++ catalog/views.py
@@ -1,15 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from catalog.models import Product
 
 
-# Create your views here.
-def home(request):
-    return render(request, "home.html")
+def products_list(request):
+	products = Product.objects.all()
+	context = {'products': products}
+	return render(request, "product_list.html", context)
 
 
-def contacts(request):
-    if request.method == "POST":
-        name = request.POST.get("name")
-        phone = request.POST.get("phone")
-        message = request.POST.get("message")
-        print(f"Ваши данные: имя - {name} телефон - {phone} сообщение - {message}")
-    return render(request, "contacts.html")
+def product_detail(request, pk):
+	product = get_object_or_404(Product, pk=pk)
+	context = {'product': product}
+	return render(request, "product_detail.html", context)
