78c630698435ec4f6ff7f840adeae1486891e4c5
melcor <melcor2006@mail.ru>
2024-05-12 13:11:21 +0300

    last fix

--- config/urls.py
+++ config/urls.py
@@ -23,4 +23,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("catalog.urls", namespace="catalog")),
+    path('catalog/', include('catalog.urls', namespace='products')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
